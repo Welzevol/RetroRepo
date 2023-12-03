2543123275623c1a011920bab8bf4c245d02fd4f
Welzevol <148000263+Welzevol@users.noreply.github.com>
2023-12-03 16:36:03 +0300

    Task1

--- Program.cs
+++ Program.cs
@@ -1,16 +1,16 @@
-﻿// : Задайте массив на 10 целых чисел. 
-// Напишите программу, которая определяет количество чётных чисел в массиве.
+﻿using System.Collections;
 
 int size = 10;
-int count = 0;
 int i = 0;
+int count = 0;
 int[] array = new int[size];
 for (i = 0; i < array.Length; i++)
 {
-    array[i] = new Random().Next(1, 200); 
-    if(array[i] % 2 == 0)
-    count++;
+    array[i] = new Random().Next(1, 101);
+
+    if (array[i] >= 20 && array[i] <= 90)
+
+        count++;
 }
 
 Console.WriteLine($"[ {string.Join("; ", array)} ] >= {count}");
-
